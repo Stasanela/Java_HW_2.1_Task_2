3b063304902d2f3c87890db37566fd67d6293fcf
stasanela <stasanela@yandex.ru>
2021-08-11 16:41:03 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int bill = 100;
+        int refill = 46769;
+        int bonus = refill / 100;
+        int totalbill = bill + bonus + refill;
+        System.out.println(totalbill);
+
+    }
+}
