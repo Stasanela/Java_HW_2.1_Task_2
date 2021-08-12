7f7556800d8c9859b83960f7821cbf3ef4580874
stasanela <stasanela@yandex.ru>
2021-08-12 14:00:42 +0300

    New commit

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,16 @@ public class Main {
     public static void main(String[] args) {
 
         int bill = 100;
-        int refill = 46769;
-        int bonus = refill / 100;
-        int totalbill = bill + bonus + refill;
-        System.out.println(totalbill);
+        int refill = 8575;
+        int bonus;
+        if (refill > 1000) {
+            bonus = refill / 100;
+        } else {
+            bonus = 0;
+        }
+        int totalBill = bill + bonus + refill;
+        System.out.println(totalBill);
+        System.out.println(bonus);
 
     }
 }
